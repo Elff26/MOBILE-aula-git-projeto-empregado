2c27d5c0e71baddde3973198662afacb9c1a0aac
Rodrigo <rod.bossini@gmail.com>
2018-11-27 15:06:32 -0200

    Primeiro teste concluído com sucesso

--- Empregado.java
+++ Empregado.java
@@ -5,6 +5,26 @@ public class Empregado{
     private double salario;
     private double comissao;
     private double bonus;
+
+    //adicionar à classe Empregado.java
+    public void setSalario (double salario){
+        this.salario = salario;
+    }
+    public double getSalario (){
+        return this.salario;
+    }
+    public void setComissao (double comissao){
+        this.comissao = comissao;
+    }
+    public double getComissao (){
+        return this.comissao;
+    }
+    public void setBonus (double bonus){
+        this.bonus = bonus;
+    }
+    public double getBonus (){
+        return this.bonus;
+    }
     public void setTipo (int tipo){
         this.tipo = tipo;
     }
